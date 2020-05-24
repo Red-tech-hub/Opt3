1240b64f058fd6b5c09550f74d285c8f8b6c7625
Red-tech-hub <64688769+Red-tech-hub@users.noreply.github.com>
2020-05-24 23:21:47 +0200

    Add files via upload

---
+++ Controller.java
@@ -0,0 +1,49 @@
+package com.company;
+
+import java.util.ArrayList;
+class Baas{
+    private static ArrayList<Werknemer> werknemers;
+    private static ArrayList<Veiligheidsmaatregelen> veiligheidsmaatregelens;
+
+
+    public static void initialize(){
+        werknemers = new ArrayList<>();
+        veiligheidsmaatregelens = new ArrayList<>();
+    }
+
+}
+
+
+class Klant {
+    Werknemer wordGeholpenDoor;
+
+    }
+}
+
+class Werknemer extends Baas {
+
+
+    }
+
+    public String taak() {
+
+    }
+
+    public ArrayList<Double> uurloon() {
+
+
+    }
+}
+
+
+class Veiligheidsmaatregelen extends Baas{
+
+    public Double maxAantalKlanten() {
+
+    }
+
+    public Integer handschoenen(){
+
+    }
+}
+
